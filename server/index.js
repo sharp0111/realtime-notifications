7a97353e93572cda232167b243b3c54885f037e6
Res Yeg <shaqk702@gmail.com>
2018-11-05 07:54:35 -0800

    add subscriptions

--- server/index.js
+++ server/index.js
@@ -2,22 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
+const { PubSub } = require('graphql-subscriptions');
+
+const pubsub = new PubSub();
+const NOTIFICATION_SUBSCRIPTION_TOPIC = 'newNotifications';
 
 const notifications = [];
 const typeDefs = `
   type Query { notifications: [Notification] }
   type Notification { label: String }
   type Mutation { pushNotification(label: String!): Notification }
+  type Subscription { newNotification: Notification }
 `;
 const resolvers = {
-  Query: { notifications: () => notifications },
-  Mutation: {
-    pushNotification: (root, args) => {
-      const newNotification = { label: args.label };
-      notifications.push(newNotification);
+    Query: { notifications: () => notifications },
+    Mutation: {
+        pushNotification: (root, args) => {
+        const newNotification = { label: args.label };
+        notifications.push(newNotification);
 
-      return newNotification;
+        return newNotification;
         },
+    },
+    Subscription: {
+        newNotification: {
+          subscribe: () => pubsub.asyncIterator(NOTIFICATION_SUBSCRIPTION_TOPIC)
+        }
     },
 };
 const schema = makeExecutableSchema({ typeDefs, resolvers });
