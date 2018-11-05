415a43140d5c4b2a67570eeff6f514cfe92882de
Res Yeg <shaqk702@gmail.com>
2018-11-05 08:03:22 -0800

    update Add Subscriptions

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 const { PubSub } = require('graphql-subscriptions');
+const cors = require('cors');
+const { execute, subscribe } = require('graphql');
+const { createServer } = require('http');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
 
 const pubsub = new PubSub();
 const NOTIFICATION_SUBSCRIPTION_TOPIC = 'newNotifications';
@@ -15,26 +19,41 @@ const typeDefs = `
   type Subscription { newNotification: Notification }
 `;
 const resolvers = {
-    Query: { notifications: () => notifications },
-    Mutation: {
-        pushNotification: (root, args) => {
+  Query: { notifications: () => notifications },
+  Mutation: {
+      pushNotification: (root, args) => {
         const newNotification = { label: args.label };
         notifications.push(newNotification);
+        pubsub.publish(NOTIFICATION_SUBSCRIPTION_TOPIC, { newNotification: newNotification });
 
         return newNotification;
-        },
-    },
-    Subscription: {
-        newNotification: {
-          subscribe: () => pubsub.asyncIterator(NOTIFICATION_SUBSCRIPTION_TOPIC)
-        }
-    },
+      },
+  },
+  Subscription: {
+    newNotification: {
+      subscribe: () => pubsub.asyncIterator(NOTIFICATION_SUBSCRIPTION_TOPIC)
+    }
+  },
 };
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express();
+app.use('*', cors({ origin: `http://localhost:3000` }));
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.listen(4000, () => {
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+  subscriptionsEndpoint: `ws://localhost:4000/subscriptions`
+}));
+const ws = createServer(app);
+ws.listen(4000, () => {
   console.log('Go to http://localhost:4000/graphiql to run queries!');
+
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: ws,
+    path: '/subscriptions',
+  });
 });
