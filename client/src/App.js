5406df38de98f6f4d6d707f10064250c7626e9b8
Res Yeg <shaqk702@gmail.com>
2018-11-05 08:28:00 -0800

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import PushNotification from 'PushNotification'
+import PushNotification from './PushNotification'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { ToastContainer, toast } from 'react-toastify';
