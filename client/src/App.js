b6010296329a0e0f7875da35d2902a0941d5a3d2
Res Yeg <shaqk702@gmail.com>
2018-11-05 08:19:11 -0800

    add subscription to get real-time notifications

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import PushNotification from 'PushNotification'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import { ToastContainer, toast } from 'react-toastify';
 
 class App extends Component {
+
+  componentWillReceiveProps({ data: { newNotification: { label } } }) {
+    toast(label);
+  }
+
   render() {
     return (
-      <div className="App-intro">
-        <PushNotification/>
+      <div className="App">
+        <div className="App-intro">
+          <PushNotification/>
+        </div>
+        <ToastContainer />
       </div>
     );
   }
 }
 
-export default App;
+const subNewNotification = gql`
+  subscription {
+    newNotification {
+      label
+    }
+  }
+`;
+
+export default graphql(subNewNotification)(App);
